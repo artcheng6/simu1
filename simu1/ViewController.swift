c05769733f2214741eb9e0c68e0a982838f5409c
ChengArthur <artcheng6@macarthur.local>
2016-10-05 11:55:56 +0800

    Initial Commit

---
+++ simu1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simu1
+//
+//  Created by ChengArthur on 2016/10/5.
+//  Copyright © 2016年 cloudexcel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
